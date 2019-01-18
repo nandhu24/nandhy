d63f1c65fb97bbedb959de141960b84d2076339f
nandhu24 <43338294+nandhu24@users.noreply.github.com>
2019-01-18 20:21:02 +0530

    Create Charater.c

---
+++ Charater.c
@@ -0,0 +1,12 @@
+#include<studio.h>
+Int main()
+{
+Char ch;
+Printf("enter the charater:\n");
+Scanf("%c",&ch);
+If(ch=='a'||ch=='A'||ch=='E'||ch=='e'||ch=='i'||ch=='I'||ch=='O'||ch=='o'||ch=='U'||ch=='u')
+Printf("%c is a vowel\n");
+else
+Printf("%c is consonant",ch);
+Return 0;
+}
